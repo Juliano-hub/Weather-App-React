a6ffad375e3bec5d70466fae048674e94d7edfcc
Juliano-hub <juliano.buss@gmail.com>
2022-02-01 19:27:42 -0300

    Getting infos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import react, {useEffect, useState} from 'react'
 import axios from 'axios'
+import reactDom from 'react-dom';
 
 function App() {
+
   const [CurrentLocation, setCurrentLocation] = useState({
     latitude: '',
     longitude: ''
@@ -17,16 +19,39 @@ function App() {
     window.alert(props.error)
   }
 
-  const [CurrentWeather, setCurrentWeather] = useState()
+  const [CurrentWeather, setCurrentWeather] = useState({
+    clouds: '',
+    temp: '',
+    feels_like: '',
+    name: '',
+    country: '',
+    weather: '',
+    timezone: ''
+  })
 
   let Weather = async(lat, lon) => {
     //console.log('In:', lat, lon)
+
     console.log(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_KEY}`)
 
     let res = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_KEY}`)
 
     console.log(res)
-    setCurrentWeather(res)
+    console.log(res.data.clouds)
+    console.log(res.data.name)
+    console.log(res.data.weather[0].description)
+
+    setCurrentWeather({...CurrentWeather, 
+      clouds: '0',
+      temp: res.data.main.temp,
+      feels_like: res.data.main.feels_like,
+      name: res.data.name,
+      country: res.data.sys.country,
+      weather: res.data.weather[0].description,
+      timezone: res.data.timezone
+    })
+
+    console.log(CurrentWeather)
   }
 
   useEffect (() =>{
@@ -35,9 +60,9 @@ function App() {
   })
 
   return (
-    <div>
-      <button className='ShowButton' onClick={() => Weather(CurrentLocation.latitude, CurrentLocation.longitude)}> Show Weather </button>
-    </div>    
+      <div className='Centering'> 
+        <button className='ShowButton' onClick={() => Weather(CurrentLocation.latitude, CurrentLocation.longitude)}> Show Weather </button>
+      </div>
   )
 }
 
---
+++ src/components/display.js
@@ -0,0 +1,13 @@
+import './App.css';
+import react, {useEffect, useState} from 'react'
+
+function Display() {
+
+  return (
+        <div> 
+            <h1>AAA</h1>
+        </div>
+  )
+}
+
+export default Display;
